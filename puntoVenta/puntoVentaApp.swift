859b761dc0e2bf1d71380df58bd02f594565b29b
Emiliano Lopez <lopezemiliano195@gmail.com>
2023-04-24 17:09:43 -0600

    Initial Commit

---
+++ puntoVenta/puntoVentaApp.swift
@@ -0,0 +1,17 @@
+//
+//  puntoVentaApp.swift
+//  puntoVenta
+//
+//  Created by ISSC_611_2023 on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct puntoVentaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
